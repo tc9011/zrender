1484e24f01c0b53d12939bcf13512cf2f72fa88f
100pah <sushuang0322@gmail.com>
2026-05-04 14:53:53 +0800

    release: 6.1.0

--- src/zrender.ts
+++ src/zrender.ts
@@ -578,7 +578,7 @@ export function registerSSRDataGetter<T>(getter: ElementSSRDataGetter<T>) {
 /**
  * @type {string}
  */
-export const version = '6.0.0';
+export const version = '6.1.0';
 
 
 export interface ZRenderType extends ZRender {};
